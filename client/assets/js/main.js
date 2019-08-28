5cdabb829042c6c93d6ade2dffed06578d8a9680
Mitchell O'Hair <mitchellohair@gmail.com>
2019-08-27 17:39:59 -0700

    Added html and styling

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -6,22 +6,20 @@ var displayArray = [];
 var stringNumberToPush = '';
 var calculationResult = null;
 
-function initializeApp () {
+function initializeApp() {
   applyClickHandlers();
 }
 
-function applyClickHandlers () {
-  $('#c-button').on('click', clearButtonHandler);
-  $('#ac-button').on('click', clearButtonHandler);
-  $('#number-block').on('click', '.number', numberButtonHandler);
-  $('#number-block').on('click', '.decimal', numberButtonHandler);
-  $('#operator-column').on('click', '.operator', operatorButtonHandler);
-  $('.negative').on('click', negativeButtonHandler);
-  $('#equals').on('click', equalsButtonHandler);
+function applyClickHandlers() {
+  $('.clear').on('click', '.c-clear, .ac-clear', clearButtonHandler);
+  $('.number-pad').on('click', '.number, .point', numberButtonHandler);
+  $('.operands').on('click', '.operator', operatorButtonHandler);
+  $('.pos-neg').on('click', negativeButtonHandler);
+  $('.equal').on('click', equalsButtonHandler);
 }
 
-function clearButtonHandler (event) {
-  var inputtedClear = $(event.currentTarget).find('p').text();
+function clearButtonHandler(event) {
+  var inputtedClear = $(event.currentTarget).text();
   if (inputtedClear === 'AC') {
     calculationArray = [];
     stringNumberToPush = '';
@@ -34,11 +32,11 @@ function clearButtonHandler (event) {
   updateDisplay();
 }
 
-function numberButtonHandler (event) {
-  var inputtedNumber = $(event.currentTarget).find('p').text();
+function numberButtonHandler(event) {
+  var inputtedNumber = $(event.currentTarget).text();
 
-  if (stringNumberToPush[stringNumberToPush.length-1] !== '.' ||
-      inputtedNumber !== '.') {
+  if (stringNumberToPush[stringNumberToPush.length - 1] !== '.' ||
+    inputtedNumber !== '.') {
     stringNumberToPush += inputtedNumber;
     displayArray.push(inputtedNumber);
 
@@ -49,13 +47,13 @@ function numberButtonHandler (event) {
 }
 
 function operatorButtonHandler(event) {
-  var inputtedOperator = $(event.currentTarget).find('p').text();
+  var inputtedOperator = $(event.currentTarget).text();
 
-  if(!displayArray.length) {
+  if (!displayArray.length) {
     return;
   }
 
-  if ('+-*/'.includes(displayArray[displayArray.length-1])) {
+  if ('+-*/'.includes(displayArray[displayArray.length - 1])) {
     displayArray.pop();
     calculationArray.pop();
   }
@@ -64,7 +62,7 @@ function operatorButtonHandler(event) {
 
   updateDisplay();
 
-  if(stringNumberToPush) {
+  if (stringNumberToPush) {
     calculationArray.push(stringNumberToPush);
   }
   calculationArray.push(inputtedOperator);
@@ -73,11 +71,11 @@ function operatorButtonHandler(event) {
   lastOperation = [];
 }
 
-function negativeButtonHandler () {
-  if(stringNumberToPush[stringNumberToPush.length-1] === '-') {
+function negativeButtonHandler() {
+  if (stringNumberToPush[stringNumberToPush.length - 1] === '-') {
     stringNumberToPush = '';
     displayArray.pop();
-  } else if (!stringNumberToPush.length || '+-*/'.includes(stringNumberToPush[stringNumberToPush.length-1])){
+  } else if (!stringNumberToPush.length || '+-*/'.includes(stringNumberToPush[stringNumberToPush.length - 1])) {
     stringNumberToPush += '-';
     displayArray.push('-');
   }
@@ -103,7 +101,7 @@ function equalsButtonHandler() {
 
   calculationResult = solve();
   if (calculationResult == 'Infinity' ||
-      calculationResult == '-Infinity') {
+    calculationResult == '-Infinity') {
     calculationResult = 'Error';
   }
   displayArray.push(calculationResult);
@@ -112,15 +110,15 @@ function equalsButtonHandler() {
   updateDisplay();
 }
 
-function updateDisplay () {
+function updateDisplay() {
   var displayText = displayArray.join('');
-  $('#display-text').text(displayText);
+  $('.display').text(displayText);
 }
 
-function solve () {
+function solve() {
   for (var multDivIndex = 1; multDivIndex < calculationArray.length; ++multDivIndex) {
-    if (calculationArray[ multDivIndex ] === '*' ||
-        calculationArray[ multDivIndex ] === '/') {
+    if (calculationArray[multDivIndex] === '*' ||
+      calculationArray[multDivIndex] === '/') {
       if (!calculationArray[multDivIndex + 1]) {
         calculationArray[multDivIndex + 1] = calculate(calculationArray[multDivIndex - 1], calculationArray[multDivIndex - 1], calculationArray[multDivIndex]);
       } else {
@@ -132,8 +130,8 @@ function solve () {
   }
 
   for (var addSubIndex = 1; addSubIndex < calculationArray.length; ++addSubIndex) {
-    if (calculationArray[ addSubIndex ] === '+' ||
-      calculationArray[ addSubIndex ] === '-') {
+    if (calculationArray[addSubIndex] === '+' ||
+      calculationArray[addSubIndex] === '-') {
       if (!calculationArray[addSubIndex + 1]) {
         calculationArray[addSubIndex + 1] = calculate(calculationArray[addSubIndex - 1], calculationArray[addSubIndex - 1], calculationArray[addSubIndex]);
       } else {
@@ -148,7 +146,7 @@ function solve () {
   return calculationArray[0];
 }
 
-function calculate (num1, num2, operator) {
+function calculate(num1, num2, operator) {
   var number1 = parseFloat(num1);
   var number2 = parseFloat(num2);
   var result = null;
@@ -170,3 +168,178 @@ function calculate (num1, num2, operator) {
 
   return result;
 }
+
+
+
+// $(document).ready(initializeApp);
+
+// var lastOperation = [];
+// var calculationArray = [];
+// var displayArray = [];
+// var stringNumberToPush = '';
+// var calculationResult = null;
+
+// function initializeApp () {
+//   applyClickHandlers();
+// }
+
+// function applyClickHandlers () {
+//   $('#c-button').on('click', clearButtonHandler);
+//   $('#ac-button').on('click', clearButtonHandler);
+//   $('#number-block').on('click', '.number', numberButtonHandler);
+//   $('#number-block').on('click', '.decimal', numberButtonHandler);
+//   $('#operator-column').on('click', '.operator', operatorButtonHandler);
+//   $('.negative').on('click', negativeButtonHandler);
+//   $('#equals').on('click', equalsButtonHandler);
+// }
+
+// function clearButtonHandler (event) {
+//   var inputtedClear = $(event.currentTarget).find('p').text();
+//   if (inputtedClear === 'AC') {
+//     calculationArray = [];
+//     stringNumberToPush = '';
+//     calculationResult = null;
+//     displayArray = [];
+//     lastOperation = [];
+//   } else {
+//     displayArray.pop();
+//   }
+//   updateDisplay();
+// }
+
+// function numberButtonHandler (event) {
+//   var inputtedNumber = $(event.currentTarget).find('p').text();
+
+//   if (stringNumberToPush[stringNumberToPush.length-1] !== '.' ||
+//       inputtedNumber !== '.') {
+//     stringNumberToPush += inputtedNumber;
+//     displayArray.push(inputtedNumber);
+
+//     updateDisplay();
+//   }
+
+//   lastOperation = [];
+// }
+
+// function operatorButtonHandler(event) {
+//   var inputtedOperator = $(event.currentTarget).find('p').text();
+
+//   if(!displayArray.length) {
+//     return;
+//   }
+
+//   if ('+-*/'.includes(displayArray[displayArray.length-1])) {
+//     displayArray.pop();
+//     calculationArray.pop();
+//   }
+
+//   displayArray.push(inputtedOperator);
+
+//   updateDisplay();
+
+//   if(stringNumberToPush) {
+//     calculationArray.push(stringNumberToPush);
+//   }
+//   calculationArray.push(inputtedOperator);
+
+//   stringNumberToPush = '';
+//   lastOperation = [];
+// }
+
+// function negativeButtonHandler () {
+//   if(stringNumberToPush[stringNumberToPush.length-1] === '-') {
+//     stringNumberToPush = '';
+//     displayArray.pop();
+//   } else if (!stringNumberToPush.length || '+-*/'.includes(stringNumberToPush[stringNumberToPush.length-1])){
+//     stringNumberToPush += '-';
+//     displayArray.push('-');
+//   }
+
+//   updateDisplay();
+// }
+
+// function equalsButtonHandler() {
+
+//   if (calculationArray.length && !lastOperation.length) {
+//     lastOperation.push(calculationArray[calculationArray.length - 1], stringNumberToPush);
+//   }
+
+//   if (calculationResult && lastOperation.length) {
+//     calculationArray = [];
+//     calculationArray.push(calculationResult, lastOperation[0], lastOperation[1]);
+//   } else {
+//     calculationArray.push(stringNumberToPush);
+//   }
+
+//   stringNumberToPush = '';
+//   displayArray = [];
+
+//   calculationResult = solve();
+//   if (calculationResult == 'Infinity' ||
+//       calculationResult == '-Infinity') {
+//     calculationResult = 'Error';
+//   }
+//   displayArray.push(calculationResult);
+//   calculationArray = [];
+
+//   updateDisplay();
+// }
+
+// function updateDisplay () {
+//   var displayText = displayArray.join('');
+//   $('#display-text').text(displayText);
+// }
+
+// function solve () {
+//   for (var multDivIndex = 1; multDivIndex < calculationArray.length; ++multDivIndex) {
+//     if (calculationArray[ multDivIndex ] === '*' ||
+//         calculationArray[ multDivIndex ] === '/') {
+//       if (!calculationArray[multDivIndex + 1]) {
+//         calculationArray[multDivIndex + 1] = calculate(calculationArray[multDivIndex - 1], calculationArray[multDivIndex - 1], calculationArray[multDivIndex]);
+//       } else {
+//         calculationArray[multDivIndex + 1] = calculate(calculationArray[multDivIndex - 1], calculationArray[multDivIndex + 1], calculationArray[multDivIndex]);
+//       }
+//       calculationArray.splice(multDivIndex - 1, 2);
+//       --multDivIndex;
+//     }
+//   }
+
+//   for (var addSubIndex = 1; addSubIndex < calculationArray.length; ++addSubIndex) {
+//     if (calculationArray[ addSubIndex ] === '+' ||
+//       calculationArray[ addSubIndex ] === '-') {
+//       if (!calculationArray[addSubIndex + 1]) {
+//         calculationArray[addSubIndex + 1] = calculate(calculationArray[addSubIndex - 1], calculationArray[addSubIndex - 1], calculationArray[addSubIndex]);
+//       } else {
+//         calculationArray[addSubIndex + 1] = calculate(calculationArray[addSubIndex - 1], calculationArray[addSubIndex + 1], calculationArray[addSubIndex]);
+//       }
+//       calculationArray.splice(addSubIndex - 1, 2);
+//       --addSubIndex;
+//     }
+//   }
+
+//   stringNumberToPush += calculationArray[0];
+//   return calculationArray[0];
+// }
+
+// function calculate (num1, num2, operator) {
+//   var number1 = parseFloat(num1);
+//   var number2 = parseFloat(num2);
+//   var result = null;
+
+//   switch (operator) {
+//     case '+':
+//       result = number1 + number2;
+//       break;
+//     case '-':
+//       result = number1 - number2;
+//       break;
+//     case '*':
+//       result = number1 * number2;
+//       break;
+//     case '/':
+//       result = number1 / number2;
+//       break;
+//   }
+
+//   return result;
+// }
